142df48665ad418084217e72e4edda5fe6b599f0
Kingtous <me@kingtous.cn>
2021-03-18 00:54:33 +0800

    fix: 解决webdemo问题

--- module/web/src/webdemo.cpp
+++ module/web/src/webdemo.cpp
@@ -1,17 +1,18 @@
 //
 // Created by 金韬 on 2021/3/8.
 //
+#include <iostream>
 
 #include "web.hpp"
 
-int main(){
+int main() {
     auto ip = "39.107.226.2";
     auto port = "80";
     int socketId = _web_getSocket();
-    int code = _web_connectSocket(socketId,ip,port);
+    int code = _web_connectSocket(socketId, ip, port);
 //    auto resp = _web_callGetRequest(socketId,"file.kingtous.cn","/");
-    auto resp = _web_callPostRequest(socketId,"file.kingtous.cn","/","name=root");
-    cout<<resp;
+    auto resp = _web_callPostRequest(socketId, "file.kingtous.cn", "/", "name=root");
+    std::cout << resp;
     _web_closeSocket(socketId);
     return code;
 }
