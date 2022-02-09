c252f836ceba305a2afaeec3658a88133cc9eb3b
ggssh <2821764200@qq.com>
2022-02-09 18:43:05 +0800

    update ui

--- pass/TimeAnalysisPass.cpp
+++ pass/TimeAnalysisPass.cpp
@@ -20,14 +20,14 @@ bool TimeAnalysisPass::runOnFunction(Function &func) {
 
 //    auto st_mem = Builder->CreateAlloca(getTypeFromStr("long"),NIL,LabelPrefix+string("st"));
     auto call_v = Builder->CreateCall(ExternFunctionHandler::getOrAddTimeFunc(*TheContext, *TheModule),
-                                     ConstantExpr::getNullValue(getTypeFromStr("long")->getPointerTo()));
+                                      ConstantExpr::getNullValue(getTypeFromStr("long")->getPointerTo()));
     auto call_v_mem = Builder->CreateAlloca(getTypeFromStr("long"));
     Builder->CreateStore(call_v, call_v_mem);
 
     auto bb_it_end = func.getBasicBlockList().rbegin();
     Builder->SetInsertPoint(&(*bb_it_end->rbegin()));
     auto call_v_2 = Builder->CreateCall(ExternFunctionHandler::getOrAddTimeFunc(*TheContext, *TheModule),
-                                       ConstantExpr::getNullValue(getTypeFromStr("long")->getPointerTo()));
+                                        ConstantExpr::getNullValue(getTypeFromStr("long")->getPointerTo()));
     auto call_v_2_mem = Builder->CreateAlloca(getTypeFromStr("long"));
     Builder->CreateStore(call_v_2, call_v_2_mem);
 
@@ -35,16 +35,18 @@ bool TimeAnalysisPass::runOnFunction(Function &func) {
     auto time_mem = Builder->CreateAlloca(getTypeFromStr("long"));
     Builder->CreateStore(time_v, time_mem);
 
-    auto stmt = TheModule->getGlobalVariable(LabelPrefix + string(func.getName()) +  string("time_cost"));
+    auto stmt = TheModule->getGlobalVariable(LabelPrefix + string(func.getName()) + string("time_cost"));
     if (stmt == NIL) {
-        stmt = Builder->CreateGlobalString(string("function<"+func.getName().str())+string(">: %ld(ms)\n"), LabelPrefix + string("time_cost"));
+        stmt = Builder->CreateGlobalString(string("function<" + func.getName().str()) + string(">: %ld(ms)\n"),
+                                           LabelPrefix + string("time_cost"));
     }
     std::vector<Constant *> args_vec;
     args_vec.push_back(ConstantInt::get(getTypeFromStr("int"), 0));
     args_vec.push_back(ConstantInt::get(getTypeFromStr("int"), 0));
-    auto stmt_ptr = Builder->CreateInBoundsGEP(stmt,{ConstantInt::get(getTypeFromStr("int"),0),ConstantInt::get(getTypeFromStr("int"),0)});
+    auto stmt_ptr = Builder->CreateInBoundsGEP(stmt, {ConstantInt::get(getTypeFromStr("int"), 0),
+                                                      ConstantInt::get(getTypeFromStr("int"), 0)});
     Builder->CreateCall(printfFunc,
-                       {stmt_ptr, Builder->CreateLoad(time_mem)});
+                        {stmt_ptr, Builder->CreateLoad(time_mem)});
     return false;
 }
 
